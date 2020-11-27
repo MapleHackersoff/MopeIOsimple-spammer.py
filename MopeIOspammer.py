5fdb7b11caca30976fa8be77d374944b27f01bc2
%RandomUser% <68478332+MapleHackersoff@users.noreply.github.com>
2020-11-27 19:51:46 +0200

    Add files via upload

---
+++ MopeIOspammer.py
@@ -0,0 +1,64 @@
+from keyboard import press_and_release
+from time import sleep
+import pyautogui as p, pyperclip
+import os
+
+os.system("echo Ready!")
+
+
+
+p.press("Enter")
+def paste(text001: str):
+    pyperclip.copy(text001)
+    press_and_release('ctrl + v')
+
+paste("FlexHack")
+p.press("Enter")
+#
+p.press("Enter")
+def paste(text002: str):
+    pyperclip.copy(text002)
+    press_and_release('ctrl + v')
+
+paste("I`m not hacking xD")
+p.press("Enter")
+#
+p.press("Enter")
+def paste(text003: str):
+    pyperclip.copy(text003)
+    press_and_release('ctrl + v')
+
+paste("https://discord.gg/75w5fK8")
+p.press("Enter")
+#
+p.press("Enter")
+def paste(text004: str):
+    pyperclip.copy(text004)
+    press_and_release('ctrl + v')
+
+paste("Dude, download our hack!")
+p.press("Enter")
+
+p.press("Enter")
+def paste(text005: str):
+    pyperclip.copy(text005)
+    press_and_release('ctrl + v')
+
+paste("Hack by Jau#1119")
+p.press("Enter")
+sleep(3)
+p.press("Enter")
+def paste(text006: str):
+    pyperclip.copy(text006)
+    press_and_release('ctrl + v')
+
+paste("Download here:")
+p.press("Enter")
+
+p.press("Enter")
+def paste(text007: str):
+    pyperclip.copy(text007)
+    press_and_release('ctrl + v')
+
+paste("invite.gg/Maple")
+p.press("Enter")
